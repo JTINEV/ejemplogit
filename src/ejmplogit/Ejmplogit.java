3d6fe3215c91ee2d323c977a60ec9bbacdb9d042
Juana Valentina Plaza <juana.plaza01@uceva.edu.co>
2023-09-19 21:51:01 -0500

    Este es el primer commit escrito en Netbeans

---
+++ src/ejmplogit/Ejmplogit.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package ejmplogit;
+
+/**
+ *
+ * @author Hp
+ */
+public class Ejmplogit {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
